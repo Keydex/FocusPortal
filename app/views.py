90fb079f26bedee73167ad4dd18a2acaba7c49c2
Anthony Pham <anpham2@illinois.edu>
2017-02-25 16:39:24 -0600

    added buttons for categories

--- app/views.py
+++ app/views.py
@@ -5,12 +5,14 @@ from .models import *
 def index(request):
         if request.method == 'POST':
                 q = request.POST.get('code')
+		print q
                 t = ['java','python']
                 tit = request.POST.get('title')
+		print tit
                 post = Post(title=tit, tag=t, code=q)
                 post.save()
-        content = {"python": Code.objects.get(category="python"), "java":Code.objects.get(category="java"), "C":Code.objects.get(category="c")};
-        return render(request, "index.html", content)
+        #content = {"python": Code.objects.get(category="python"), "java":Code.objects.get(category="java"), "C":Code.objects.get(category="c")};
+        return render(request, "index.html")
 
 def login(request):
         return render(request, "login.html")
