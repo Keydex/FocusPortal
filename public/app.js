85b037cf460d7ec20511f06aea5bc15ce21418bb
Anthony Pham <anpham2@illinois.edu>
2017-02-26 02:12:30 -0600

    firebase setup

--- public/app.js
+++ public/app.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var firebase = require('firebase');
+var firebaseui = require('firebaseui');
 
 app.use(express.static(__dirname + '/public'));
 var port = process.env.PORT || 3000;
